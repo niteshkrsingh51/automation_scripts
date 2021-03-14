03aa0a6e5b705b4b80ffd331e02f4ef4b380bc4f
Nitesh <niteshkrsingh51@gmail.com>
2021-03-14 22:17:16 +0530

    Update script1.py

--- automate_excel/script1.py
+++ automate_excel/script1.py
@@ -10,3 +10,13 @@ df_second_shift = pd.read_excel(excel_file_1, sheet_name='second')
 df_third_shift = pd.read_excel(excel_file_2)
 
 print(df_first_shift['Product'])
+
+df_all = pd.concat([df_first_shift,df_second_shift,df_third_shift])
+print(df_all)
+
+pivot = df_all.groupby(['Shift']).mean()
+shift_productivity = pivot.loc[:,'Production Run Time (Min)':'Products Produced (Units)']
+print(shift_productivity)
+
+shift_productivity.plot(kind='bar')
+plt.show()
